61f94c6166343b5ebf4b9eb4ccd3afadfae0bb07
Rejoy Dominic <rejoydominic@gmail.com>
2015-10-13 09:47:32 +1100

    Refactored for multiroom

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -7,21 +7,21 @@
 
   function routeConfig($routeProvider) {
     $routeProvider
-      .when('/', {
+      .when('/main', {
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
       })
-      .when('/race/', {
+      .when('/race/:raceName', {
         templateUrl: 'app/race/race.html',
         controller: 'RaceController',
         controllerAs: 'race'
       })
-      .when('/race/:raceName', {
-        templateUrl: 'app/race/race_user_selector.html',
-        controller: 'RaceUserSelectorController',
-        controllerAs: 'userSelection'
-      })
+    //   .when('/race/:raceName', {
+    //     templateUrl: 'app/race/race_user_selector.html',
+    //     controller: 'RaceUserSelectorController',
+    //     controllerAs: 'userSelection'
+    //   })
       .otherwise({
         redirectTo: '/'
       });
--- src/app/race/raceUserSelector.controller.js
+++ src/app/race/raceUserSelector.controller.js
@@ -6,17 +6,11 @@
     .controller('RaceUserSelectorController', RaceUserSelectorController);
 
   /** @ngInject */
-  function RaceUserSelectorController(realtime, $location, $routeParams) {
+  function RaceUserSelectorController( $location, $routeParams) {
     var vm = this;
 
-	vm.raceName = "aaaqqqqq";
-	vm.raceRoomsData = realtime.getRoomData($routeParams.raceName);
-	vm.selectedRaceRoom = $routeParams.raceName;
-    vm.selectedHorse = null;
+	vm.selectedRace = $routeParams.raceName;
 
-    vm.selectHorse = function(horse){
-        vm.selectedHorse = horse;
-    }
 
 
   }
